7a04763b8d755de91f98b12f9e7b266390dc9835
Mehul Daruka <mehuldaruka32@gmail.com>
2020-05-13 13:13:48 -0500

    Organized login screen.

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -10,87 +10,75 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
+  final background = BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage('images/background2.png'),
+      fit: BoxFit.cover,
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
+    ),
+  );
+
+  final phoneInput = Container(
+      width: 320,
+      child: TextField(
+        keyboardType: TextInputType.phone,
+        decoration: InputDecoration(
+            hintText: "Phone Number",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            focusedBorder:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            contentPadding: const EdgeInsets.all(16.0),
+            prefixIcon: Icon(Icons.phone, color: Colors.black)),
+      ));
+
+  final continueButton = RaisedButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(21)),
+        padding:
+            const EdgeInsets.only(top: 20, bottom: 20),
+        onPressed: () => {},
+        color: Color(0xFF2eb092),
+        textColor: Colors.white,
+        child: Text(
+          "CONTINUE",
+          style: GoogleFonts.firaSans(fontSize: 20),
+        ),
+      );
+
+  final phoneMessage = 
+      Text(
+        "We will be sending you a code to verify your phone. Standard rates may apply.",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.nanumGothic(
+            fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+      );
+
+  final mainTitle = Text(
+    "ENTER YOUR PHONE NUMBER",
+    style: GoogleFonts.nanumGothic(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: SingleChildScrollView(
-        child: DecoratedBox(
-          position: DecorationPosition.background,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/background2.png'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.7), BlendMode.dstATop),
-            ),
-          ),
-          child: Center(child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          alignment: Alignment.topCenter,
+          decoration: background,
+          padding: EdgeInsets.all(25.0),
+          child: ListView(
             children: <Widget>[
               SizedBox(height: 80),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "ENTER YOUR PHONE NUMBER",
-                      style: GoogleFonts.nanumGothic(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    )
-                  ]),
-              Padding(
-                  padding: EdgeInsets.only(left: 50, right: 50, top: 30, bottom: 30),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                            child: Text(
-                          "We will be sending you a code to verify your phone. Standard rates may apply.",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.nanumGothic(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                        ))
-                      ])),
-              SizedBox(height: 8),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        width: 320,
-                        child: TextField(
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                              hintText: "Phone Number",
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15)),
-                              contentPadding: const EdgeInsets.all(16.0),
-                              prefixIcon:
-                                  Icon(Icons.phone, color: Colors.black)),
-                        ))
-                  ]),
-              Padding(
-                  padding: const EdgeInsets.only(top: 90, bottom: 380),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(21)),
-                    padding: const EdgeInsets.only(
-                        left: 30, right: 30, top: 20, bottom: 20),
-                    onPressed: () => {},
-                    color: Color(0xFF2eb092),
-                    textColor: Colors.white,
-                    child: Text(
-                      "CONTINUE",
-                      style: GoogleFonts.firaSans(fontSize: 20),
-                    ),
-                  ))
+              mainTitle,
+              phoneMessage,
+              phoneInput,
+              continueButton,
             ],
-          )),
+          ),
         ),
       ),
     );
