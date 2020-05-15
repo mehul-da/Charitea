a5345cbef3eb5153fe9ee7ce7d5064e31118cc98
Mehul Daruka <mehuldaruka32@gmail.com>
2020-05-15 00:45:58 -0500

    Added features to SearchTab screen.

--- lib/SearchTab.dart
+++ lib/SearchTab.dart
@@ -24,15 +24,35 @@ class _SearchTabState extends State<SearchTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: Container(
-            height: MediaQuery.of(context).size.height,
-            alignment: Alignment.topCenter,
-            decoration: background,
-            child: Text("SEARCH TAB!")),
-      ),
-    );
+        resizeToAvoidBottomPadding: false,
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Container(
+              height: MediaQuery.of(context).size.height,
+              alignment: Alignment.topCenter,
+              decoration: background,
+              padding: EdgeInsets.all(23.0),
+              child: Center(
+                  child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(21)),
+                        padding: const EdgeInsets.only(
+                            top: 17, bottom: 17, left: 30, right: 30),
+                        color: Color(0xFF2eb092),
+                        onPressed: () => {},
+                        child: Text(
+                          "CONTINUE",
+                          style: GoogleFonts.firaSans(fontSize: 20),
+                        ),
+                      )),
+                ],
+              ))),
+        ));
   }
 }
